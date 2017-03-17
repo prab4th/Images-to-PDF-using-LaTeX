8287fb54fb61bbe477aa93cdaf17965f63e4af83
Prabath Swarna <prab4th@users.noreply.github.com>
2017-03-17 17:21:06 +0530

    JPEG order now needs to be specified
    The order in which the JPEGs appear should be specified in the file name.
    ex: 1_1.JPEG, astarst_2.JPEG

--- script.py
+++ script.py
@@ -5,10 +5,18 @@ from os.path import isfile, join
 dir = str(os.path.realpath(__file__))[:-9]
 onlyfiles = [f for f in listdir(dir) if isfile(join(dir, f))]
 
-for x in sorted(onlyfiles):
-    print(x)
 
-print(onlyfiles)
+import re
+
+n = 0
+pic_files = list()
+
+for f in onlyfiles:
+    if '_' in f:
+        pic_files.append(f)
+
+sorted_files = sorted(pic_files, key = lambda k: int(k.split('_')[1].split('.')[0]))
+
 with open('1.tex', 'w',) as a:
     a.write("")    
 
@@ -19,8 +27,8 @@ with open('1.tex', 'a', encoding='utf-8') as w:
 \\begin{document}
     \\title{BT4007}
     \\maketitle""")
-    for item in onlyfiles:
-        if item[-3:] == 'peg':
+    for item in sorted_files:
+        if item[-3:] == 'peg' or item[-3:] == 'jpg':
             w.writelines("""
         \\begin{slide}
             \\includegraphics[width=\\textwidth, height=\\textheight] {%s}
